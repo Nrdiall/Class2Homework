ccc368c7cd79c78bfc30263e5c179c8922b0dabf
Naython <Naython@10.30.22.82>
2017-08-30 17:11:10 -0500

    This is when the main program was written.

--- src/class2homework/Class2Homework.java
+++ src/class2homework/Class2Homework.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package class2homework;
-
+import java.util.Scanner;
 /**
  *
  * @author Naython
@@ -15,6 +15,19 @@ public class Class2Homework {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("What is your first name?");
+        String FirstName = keyboard.next();
+        System.out.print("What is your last name?");
+        String LastName = keyboard.next();
+        System.out.print("What is your first name?");
+        String FirstName2 = keyboard.next();
+        System.out.print("What is your last name?");
+        String LastName2 = keyboard.next();
+        Person p = new Person(FirstName, LastName);
+        Person c = new Person(FirstName2, LastName2);
+        System.out.println(p.getFirstName() + " " + p.getLastName());
+        System.out.println(c.getFirstName() + " " + c.getLastName());
         
     }
     
